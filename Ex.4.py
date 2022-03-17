82f52fa0ea58e37ca0443fcb77fba121ddc82f12
友人Ｓ☄プロジェクトCSJ所属 <yuujins@project.csj>
2022-03-17 12:33:19 +0900

    20220317 Update

--- Ex.4.py
+++ Ex.4.py
@@ -4,3 +4,14 @@ Name:幽霊Ｓ☄
 SID:B10813180
 Title:Ex.4
 '''
+'''
+Q1.
+
+'''
+
+import csv
+csvfile = "./data.csv"
+with open(csvfile, 'r') as data:
+    reader = csv.reader(data)
+    for row in reader:
+        print(','.join(row))
